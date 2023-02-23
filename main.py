d079e2f456bbb96f38d14faeca905cef582ad6c7
hkuribayashi <hkuribayashi@gmail.com>
2023-02-23 10:19:57 -0300

    Aula de 23/02

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-import sys
+import random
+
 
 alfabeto = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def criptografar(mensagem_plana, chave_c):
     msg_criptografada = ""
+    for letra in mensagem_plana:
+        posicao = alfabeto.index(letra)
+        nova_posicao = (posicao + chave) % 26
+        msg_criptografada += alfabeto[nova_posicao]
     return msg_criptografada
 
 
 def decriptografar(mensagem_criptografada, chave):
     msg_plana = ""
+    for letra in mensagem_criptografada:
+        posicao = alfabeto.index(letra)
+        nova_posicao = (posicao - chave) % 26
+        msg_plana += alfabeto[nova_posicao]
     return msg_plana
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print(criptografar("zhugo", 3))
-        print(decriptografar("ckxjr", 3))
-        # modo = sys.argv[1]
-        # chave = int(sys.argv[2])
-        # msg = sys.argv[3]
-        # if modo == "crypt":
-        #     print(criptografar(msg, chave))
-        # elif modo == "decrypt":
-        #     print(decriptografar(msg, chave))
-    else:
-        print("Uso: $ python main.py [crypt|decrypt] <chave> <mensagem>")
+    # Processo de Cifragrem
+    chave = random.randint(1, 26)
+    mensagem = "hugo"
+    msg_criptografada = criptografar(mensagem, chave)
+    print(msg_criptografada)
+
+    # Tentando decodificar a msg sem saber o valor da chave
+    print("Mensagem Criptografada: {}".format(msg_criptografada))
+    for chave in range(26):
+        mensagem_plana = decriptografar(msg_criptografada, chave)
+        print("Chave: {} | Mensagem: {}".format(chave, mensagem_plana))
